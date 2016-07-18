61c288eca8328074026abe0aa790d78d921e455f
athena2stacy <athena.stacy@gmail.com>
2016-07-18 01:56:41 -0700

    Updating plot files and graph?.html files.

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect
 app = Flask(__name__)
 
 @app.route('/hello_page')
@@ -7,7 +7,18 @@ def hello_world():
     # note that the function name and the route argument
     # do not need to be the same.
     return 'Hello world!'
+
+@app.route('/figure1')
+def uploaded_file():
+    filename = 'n_vs_date.png'
+    return render_template('graph1.html', filename=filename)
+
+@app.route('/figure2')
+def uploaded_file2():
+    filename = 'barchart.png'
+    return render_template('graph2.html', filename=filename)
+
     
 if __name__ == '__main__':
-#	app.run(port=33507)
-       app.run()
+	app.run(port=33507)
+#       app.run()
